6cb167dda8169a40655fe5317ea8363b4744f3fe
Savitha M <129962736+savitha08@users.noreply.github.com>
2024-05-28 21:37:55 +0530

    Complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,6 +18,8 @@ import {
 } from '../redux/user/userSlice';
 import {useDispatch} from 'react-redux';
 
+import {Link} from 'react-router-dom';
+
 // firebase storage
   // allow read;
   // allow write: if
@@ -191,6 +193,11 @@ export default function Profile() {
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'Loading...' : 'Update'}
         </button>
+
+        <Link
+         className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+         to={"/create-listing"}>Create Listing</Link>
+
       </form>
       <div className='flex justify-between mt-5'>
         <span

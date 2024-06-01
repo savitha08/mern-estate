87297e1f138dcd4454dbaf8deb8cf10d8e7a7f23
Savitha M <129962736+savitha08@users.noreply.github.com>
2024-06-01 13:21:11 +0530

    khjh

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,11 +1,10 @@
-import Listing from "../models/listing.model.js";
+import Listing from '../models/listing.model.js';
 
-export const createListing = async (req,res,next)=>{
-    try{
-        const listing = await Listing.create(req.body);
-        return res.status(201).json(listing);
-    }catch(e){
-        next(e);
-    }
-
-}
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (e) {
+    next(e);
+  }
+};

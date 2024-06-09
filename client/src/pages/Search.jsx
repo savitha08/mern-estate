04009df858e5ce8736924df9a4703a4c461bc0d6
Savitha M <129962736+savitha08@users.noreply.github.com>
2024-06-09 09:44:46 +0530

    Add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -18,6 +18,7 @@ export default function Search() {
 
   const [loading,setLoading] = useState(false);
   const [listings,setListings] = useState([]);
+  const [showMore,setShowMore] = useState(false);
   console.log(listings);
 
 
@@ -54,9 +55,15 @@ export default function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if(data.length >8){
+        setShowMore(true);
+      }else{
+        setShowMore(false);
+      }
       setListings(data);
       setLoading(false);
     };
@@ -126,6 +133,21 @@ export default function Search() {
     urlParams.set('order', sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex',startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if(data.length <9){
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
   };
 
 
@@ -284,6 +306,18 @@ export default function Search() {
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
+
+           
+           {
+            showMore && (
+              <button className='text-green-700 hover:underline p-7 text-center w-full'
+              onClick={onShowMoreClick} >
+                Show more
+              </button>
+            )
+           }
+
+
         </div>
       </div>
       
